a49b1fdd5c29ee8d0fd0ded1b92e8ed26db81e90
RobertDetaille <53101315+RobertDetaille@users.noreply.github.com>
2022-02-11 03:39:29 +0100

    Update phi.m

--- phi.m
+++ phi.m
@@ -1,5 +1,5 @@
 function [x] = phi(a,b,c,gamma,my,ny)
-% objective function of the discretized and regualized dual problem
+% objective function of the discretized dual problem
 
 A = max((a'+b)-c,0);
 x = 0.5*(norm(A, 'fro'))^2 - gamma*(dot(my,a) + dot(ny,b));
