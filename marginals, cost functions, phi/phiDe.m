7686c2cefd02092d99409eea91a677b081cc0a13
RobertDetaille <53101315+RobertDetaille@users.noreply.github.com>
2022-02-11 02:52:53 +0100

    Delete phiDe.m

--- marginals, cost functions, phi/phiDe.m
+++
@@ -1,7 +0,0 @@
-function [x] = phiDe(a,b,c,gamma,my,ny,eps,M,N)
-% objective function of the discretized and regualized dual problem
-
-A = max((a'+b)-c,0);
-x = 0.5*(norm(A, 'fro'))^2 - gamma*(dot(my,a) + dot(ny,b)) ...
-    + (eps/(M*2))*(norm(a)^2) + (eps/(N*2))*(norm(b)^2);
-end
